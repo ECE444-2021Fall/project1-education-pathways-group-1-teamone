6616401636d16ab5339e2fabd66c69ec5ac95a40
GurmeharS <gurmehar.sandhu@mail.utoronto.ca>
2021-10-27 17:11:13 -0400

    Added tests for upload_courses.py; minor restructure to folders

--- util/upload_courses.py
+++ util/upload_courses.py
@@ -7,18 +7,22 @@ sys.path.append('./modules')
 from CourseTable import CourseTable
 from DiscussionTable import DiscussionTable
 
-
-if __name__ == "__main__":
-    course_table = CourseTable()
-    discussion_table = DiscussionTable()
+def get_pickle_df(pickle_file, index):
     # Read the pickle containing the course data 
-    df = pd.read_pickle('resources/df_processed.pickle').set_index('Code')
+    df = pd.read_pickle(pickle_file).set_index(index)
     # Replace any NaN's with empty strings
     df = df.replace(np.nan, "")
-    # Only upload 20 courses for now 
-    num_courses = 20
-    i = 0
+    return df
+
+def get_courses_table():
+    return CourseTable()
+
+def get_discussion_table():
+    return DiscussionTable()
+
+def upload_courses(df, course_table, discussion_table, num_courses=20):
     # Read each course, save the nessecary fields, and call the CourseImporter
+    i = 0
     for index, course in df.iterrows():
         i += 1
         if i > num_courses:
@@ -41,4 +45,14 @@ if __name__ == "__main__":
         print(f"Creating {index}")
         course_table.add_item(params)
         discussion_table.add_item(params["CourseID"])
-        
+
+def main():
+    course_table = get_courses_table()
+    discussion_table = get_discussion_table()
+    df = get_pickle_df('resources/df_processed.pickle', 'Code')
+    
+    upload_courses(df, course_table, discussion_table, num_courses=20)
+
+if __name__ == "__main__":
+    main()
+    
