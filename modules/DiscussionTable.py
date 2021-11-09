b9500f62b84dedf72b6d66bfd48b51b2ca6c537c
griffinhadfield <griffin.hadfield@mail.utoronto.ca>
2021-11-09 13:13:12 -0500

    Added ability to make and upvote/download posts

--- modules/DiscussionTable.py
+++ modules/DiscussionTable.py
@@ -1,4 +1,6 @@
 from Table import Table
+from datetime import datetime
+import uuid
 
 # This class implements the ability to add and modify discussion boards in 
 # the discussion table in AWS
@@ -15,7 +17,7 @@ class DiscussionTable(Table):
     def add_item(self, courseID):
         discussionTableItem = {
             "CourseID": courseID,
-            "Ratings": {
+            "Likes": {
                 "Homework": 0,
                 "Content": 0,
                 "Exams": 0
@@ -28,9 +30,9 @@ class DiscussionTable(Table):
         if response['ResponseMetadata']['HTTPStatusCode'] != 200: 
             print("Error creating discussion board for:", courseID)
             print(response)
-    def get_item(self):
-        #TODO
-        pass
+    def get_item(self, courseID):
+            response = self.get_table().get_item(Key={'CourseID': courseID})
+            return response['Item']
 
     def update_item(self):
         #TODO
@@ -40,18 +42,90 @@ class DiscussionTable(Table):
         #TODO
         pass
 
-    def add_post(self):
-        #TODO
-        pass
+    def add_post(self, courseID, userName, time, content):
+        postID = str(uuid.uuid4())
+        newPost = {"PostID": postID, "UserName": userName, "Time": time, "Content": content, "NumLikes": 0}
 
-    def remove_post(self):
-        #TODO
-        pass
+        response = self.get_table().update_item(
+            Key={'CourseID': courseID},
+            UpdateExpression="SET DiscussionBoard = list_append(DiscussionBoard, :i)",
+            ExpressionAttributeValues={
+                ':i': [newPost],
+            },
+            ReturnValues="ALL_NEW"
+        )
+        return response
 
-    def upvote_post(self):
-        #TODO
-        pass
+    def get_index_of_post(self, courseID, postID):
+        discussionBoard = self.get_item(courseID)["DiscussionBoard"]
+        print(discussionBoard)
+        for idx, post in enumerate(discussionBoard):
+            if post["PostID"] == postID:
+                return idx
+        return -1
 
-    def downvote_post(self):
-        #TODO
-        pass
+    def delete_post(self, courseID, postID):
+        indexOfPost = self.get_index_of_post(courseID, postID)
+        if indexOfPost == -1:
+            return
+
+        response = self.get_table().update_item(
+            Key={'CourseID': courseID},
+            UpdateExpression=f"REMOVE DiscussionBoard[{indexOfPost}]",
+            ReturnValues="UPDATED_NEW"
+        )
+        return response
+
+
+    def upvote_post(self, courseID, postID):
+        indexOfPost = self.get_index_of_post(courseID, postID)
+        if indexOfPost == -1:
+            print("here")
+            return
+        
+        post = self.get_item(courseID)["DiscussionBoard"][indexOfPost]
+        post["NumLikes"] += 1
+
+        self.delete_post(courseID, postID)
+        print(post)
+        response = self.get_table().update_item(
+            Key={'CourseID': courseID},
+            UpdateExpression="SET DiscussionBoard = list_append(DiscussionBoard, :i)",
+            ExpressionAttributeValues={
+                ':i': [post],
+            },
+            ReturnValues="ALL_NEW"
+        )
+        return response
+
+
+    def downvote_post(self, courseID, postID):
+        indexOfPost = self.get_index_of_post(courseID, postID)
+        if indexOfPost == -1:
+            print("here")
+            return
+        
+        post = self.get_item(courseID)["DiscussionBoard"][indexOfPost]
+        post["NumLikes"] -= 1
+
+        self.delete_post(courseID, postID)
+        print(post)
+        response = self.get_table().update_item(
+            Key={'CourseID': courseID},
+            UpdateExpression="SET DiscussionBoard = list_append(DiscussionBoard, :i)",
+            ExpressionAttributeValues={
+                ':i': [post],
+            },
+            ReturnValues="ALL_NEW"
+        )
+        return response
+
+if __name__ == "__main__":
+    now = datetime.now()
+    date_time = now.strftime("%m/%d/%Y, %H:%M")
+    courseID = "GGR107H1"
+    discussion_table = DiscussionTable()
+    # print(discussion_table.add_post(courseID, "Griffin Hadfield", date_time, "This course is meh!"))
+    # print(discussion_table.delete_post(courseID, "199b4bca-4790-4a4d-8196-7d647995b602"))
+    # print(discussion_table.get_item(courseID))
+    print(discussion_table.downvote_post(courseID, "d7fce19c-5a07-428c-a080-3416dc194ffe"))
