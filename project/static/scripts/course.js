bc9d43868e7b85e8d721851b9b384ec77fb44da0
YuhuiJiang99 <yuhui.jiang@outlook.com>
2021-11-11 16:21:00 -0500

    Complete the integration with backend API for enrollment cart

--- project/static/scripts/course.js
+++ project/static/scripts/course.js
@@ -41,9 +41,7 @@ function toggleHeartColor(tag_heart, code, postID){
             // "Access-Control-Allow-Headers": "Content-Type"
         },
     }).then(response => response.json())
-    .then(data => {
-        console.log(data)
-    });
+    .catch(error => console.log(error));
 
 }
 
@@ -52,37 +50,12 @@ function countLines(ele) {
   var lh = parseInt(styles.lineHeight, 10);
   var h = parseInt(styles.height, 10);
   var lc = Math.round(h / lh);
-  console.log('line count:', lc, 'line-height:', lh, 'height:', h);
   return lc;
 }
+
 var tagManyText = document.getElementsByClassName("many-text")[0];
 var desLc =  countLines(tagManyText);
 if(desLc <= 2){
     var btnToggleText = document.getElementsByClassName("toggle-text")[0];
     btnToggleText.remove();
-}
-
-function buildTable(course){
-    var table = document.getElementById("courseInfo");
-    i = 0;
-    for(var key in course){
-        if(i >= 6){
-            return;
-        }
-        var content = course[key]
-        if(content != null && content.length > 0){
-            var row = `<tr> 
-                <td class="col-2"><h5>${key}</h5></td>`
-            if(key == "Course Description"){   
-                row += `<td class="col-8 description"> <p class="many-text">${course[key]}</p> 
-                        <span type="button" class="toggle-text" onclick="toggleText(this)">Read More</span></td></tr>`
-            }
-            else{
-                row += `<td class="col-8 description">${course[key]}</td></tr>`
-            }
-            table.innerHTML += row;
-            i += 1;
-        }
-
-    }
 }
--- project/static/scripts/enroll.js
+++ project/static/scripts/enroll.js
@@ -1,25 +1,50 @@
-function popup(){
-    document.querySelector('.bg-model').style.display = "flex";
+function popup(id){
+    document.getElementById(id).style.display = "flex";
 }
 
-function off(){
-    document.querySelector('.bg-model').style.display = "none";
+function off(e){
+    e.parentNode.parentNode.style.display = "none";
 }
 
-function render_table(){
-    var tag_paths =  document.getElementById("paths")
+function render_path(){
+
+    var tag_paths =  document.getElementById("path_selection")
     var path = tag_paths.options[tag_paths.selectedIndex].text;
     var tbody = document.getElementById("path_courses");
     tbody.innerHTML = "";
     for(let course of paths_json[path]){
-        let row = `<tr>
+        let row = `<tr id="">
                         <td class="align-middle col-2">${course["Code"]}</td>
                         <td class="align-middle col-5">${course["Name"]}</td>
                         <td class="align-middle col-3">${course["Semester"]}</td>
-                        <td class="align-middle text-center col-2"><button class="btn btn-secondary">drop</button></td>
+                        <td class="align-middle text-center col-2"><button class="btn btn-secondary" onclick="drop_course(this)">drop</button></td>
                     </tr>`;
         tbody.innerHTML += row;
     }
 }
 
-render_table()
+async function drop_course(e){
+    var courseCode = e.parentNode.parentNode.childNodes[1].innerText;
+    var ePathChoice = document.getElementById("path_choice");
+    var pathName = ePathChoice.options[ePathChoice.selectedIndex].text;   
+    
+    await fetch('http://localhost:5000/enroll/remove_course', {
+        method: 'POST',
+        body: JSON.stringify({
+            "action": "RemoveCourse",
+            "Username": "Tony",
+            "pathName": pathName,
+            "courseCode": courseCode
+        }), 
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    }
+    ).then(response => response.json())
+    .catch(error => console.log(error));
+    e.parentNode.parentNode.remove();
+}
+
+
+render_path()
+
